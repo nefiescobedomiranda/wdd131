235d8127df5e354aaa76cfedd39e7acb000125bf
nefiescobedomiranda <nescobedomiranda@byupathway.edu>
2026-05-13 21:50:39 -0500

    gh

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,6 +1,4 @@
-// Año actual
 document.getElementById("currentyear").textContent = new Date().getFullYear();
 
-// Última modificación del documento
 document.getElementById("lastModified").textContent =
   "Last Modified: " + document.lastModified;
