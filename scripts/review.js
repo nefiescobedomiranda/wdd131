693d524482fa540b290aef3c4ededa501e5ef878
nefiescobedomiranda <nescobedomiranda@byupathway.edu>
2026-05-29 19:02:37 -0500

    jyjh

--- scripts/review.js
+++ scripts/review.js
@@ -3,11 +3,8 @@ let count = localStorage.getItem("reviewCount");
 count = count ? Number(count) + 1 : 1;
 
 localStorage.setItem("reviewCount", count);
-
 document.addEventListener("DOMContentLoaded", () => {
-
     document.getElementById("counter").textContent = count;
-
     document.getElementById("year").textContent =
         new Date().getFullYear();
 });
