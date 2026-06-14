4e112ae3293c5c72de72196d3d52b776219699be
nefiescobedomiranda <nescobedomiranda@byupathway.edu>
2026-06-14 02:32:04 -0500

    sddsd

---
+++ project/scripts/script.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+  /* =========================
+     SCROLL ANIMATION (CLAVE)
+  ========================= */
+
+  const sections = document.querySelectorAll("section");
+
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      }
+    });
+  }, {
+    threshold: 0.15
+  });
+
+  sections.forEach(sec => observer.observe(sec));
+
+});
